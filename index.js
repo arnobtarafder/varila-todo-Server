401396801110501672207632010f7d55bdce9a2d
arnobtarafder <rmtarnob@gmail.com>
2022-05-19 04:19:09 +0600

    get connection to database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -8,6 +9,29 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jq9bw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const taskCollection = client.db("taskTaker").collection("tasks");
+  
+      
+      console.log("connected to database");
+    } 
+    
+    finally {
+
+    }
+  }
+  
+  run().catch(console.dir);
+
+
 
 
 app.get("/", (req, res) => {
