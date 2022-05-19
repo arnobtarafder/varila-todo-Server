52b6f40f5d8bb163bbdf7a63833abd89f95f3d26
arnobtarafder <rmtarnob@gmail.com>
2022-05-19 06:57:20 +0600

    add: verify token function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -10,6 +11,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+  
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jq9bw.mongodb.net/?retryWrites=true&w=majority`;
